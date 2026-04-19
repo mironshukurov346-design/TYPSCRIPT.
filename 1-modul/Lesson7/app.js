096ab4e22d92741c91133c91a050aab023dd6fae
mironshukurov346-design <mironshukurov346@gmail.com>
2026-04-19 11:57:32 +0500

    git

---
+++ 1-modul/Lesson7/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let admins = ['miron', 'ivan'];
+// admins.push('petr'); //Error: Property 'push' does not exist on type 'readonly string[]'.
+// admins[0] = 'petr'; //Error: Index signature in type 'readonly string[]' only permits reading.
+console.log(admins);
+//# sourceMappingURL=app.js.map
---
+++ 1-modul/Lesson7/app.ts
@@ -0,0 +1,13 @@
+let admins: readonly string[] = ['miron', 'ivan'];
+
+// admins.push('petr'); //Error: Property 'push' does not exist on type 'readonly string[]'.
+// admins[0] = 'petr'; //Error: Index signature in type 'readonly string[]' only permits reading.
+
+console.log(admins);
+
+
+// yani readonly - bu massivni o'zgartirishga ruxsat bermaydi,
+//  ya'ni yangi element qo'shish yoki mavjud elementlarni o'zgartirish mumkin emas.
+//  Bu massivni faqat o'qish uchun ishlatish uchun mo'ljallangan.
+
+// bu malumotlar xavfsizligini berish uchun ishlatiladi, masalan, agar siz biror joyda admins massivini o'zgartirishni xohlamasangiz, uni readonly qilib belgilashingiz mumkin.
