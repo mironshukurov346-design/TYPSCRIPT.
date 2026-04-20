26e27c2cb7e74edc07fe7f7a7f89afbec9b8cb6c
mironshukurov346-design <mironshukurov346@gmail.com>
2026-04-20 11:26:51 +0500

    git

--- 1-modul/Enums8/app.js
+++ 1-modul/Enums8/app.js
@@ -1,3 +1,24 @@
 "use strict";
+// Enums - directions / tepaga,pastga,chapga,orqaga
 Object.defineProperty(exports, "__esModule", { value: true });
+// enum Direction {
+//     Up,
+//     Down,
+//     Left,
+//     Right,
+// } 
+// enum Direction {
+//     up = 1,
+//     Down,
+//     Up,
+//     Left,
+//     Right,
+// }
+var Direction;
+(function (Direction) {
+    Direction["up"] = "UP";
+    Direction["down"] = "DOWN";
+    Direction["left"] = "LEFT";
+    Direction["right"] = "RIGHT";
+})(Direction || (Direction = {}));
 //# sourceMappingURL=app.js.map
--- 1-modul/Enums8/app.ts
+++ 1-modul/Enums8/app.ts
@@ -1,1 +1,29 @@
 // Enums - directions / tepaga,pastga,chapga,orqaga
+
+// enum Direction {
+//     Up,
+//     Down,
+//     Left,
+//     Right,
+// } 
+
+
+
+// enum Direction {
+//     up = 1,
+//     Down,
+//     Up,
+//     Left,
+//     Right,
+// }
+
+
+
+// enum Direction {
+//     up = 'UP',
+//     down = 'DOWN',
+//     left = 'LEFT',
+//     right = 'RIGHT',
+// }
+
+
