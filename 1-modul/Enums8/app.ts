2f014879fb59b7068510dfd81ac2fd1f63dfe7f6
mironshukurov346-design <mironshukurov346@gmail.com>
2026-04-20 11:18:10 +0500

    git

---
+++ 1-modul/Enums8/app.ts
@@ -0,0 +1,1 @@
+// Enums - directions / tepaga,pastga,chapga,orqaga
--- 1-modul/Lesson7/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let admins = ['miron', 'ivan'];
-// admins.push('petr'); //Error: Property 'push' does not exist on type 'readonly string[]'.
-// admins[0] = 'petr'; //Error: Index signature in type 'readonly string[]' only permits reading.
-console.log(admins);
-//# sourceMappingURL=app.js.map
